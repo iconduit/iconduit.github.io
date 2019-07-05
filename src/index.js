f00aba28099be6569d7b3d0f66f6b0ed2b8f62eb
Erin Millard <ezzatron@gmail.com>
2019-07-05 14:03:33 +1000

    Render into a container

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import {render} from 'react-dom'
 import App from './component/App.jsx'
 
 document.addEventListener('DOMContentLoaded', function () {
-  render(App(), document.body)
+  const container = document.createElement('span')
+  document.body.appendChild(container)
+
+  render(App(), container)
 })
