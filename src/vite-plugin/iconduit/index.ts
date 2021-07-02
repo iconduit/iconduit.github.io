a60a10515c77ac0cc1d2428fcae8136e3c9ef98d
Erin Millard <ezzatron@gmail.com>
2021-07-02 13:41:11 +1000

    WIP

--- src/vite-plugin/iconduit/index.ts
+++ src/vite-plugin/iconduit/index.ts
@@ -1,15 +1,17 @@
 import {readConsumer} from '@iconduit/consumer'
-import {join, resolve} from 'path'
+import {basename, dirname, join, resolve} from 'path'
 import {readFile} from 'fs/promises'
 import type {Plugin} from 'vite'
+import urlParse from 'url-parse'
 
 export interface ViteIconduitOptions {
   manifestPath: string
 }
 
 export function viteIconduitPlugin (options: ViteIconduitOptions): Plugin[] {
+  let assetsDir: string
   let manifestPath: string
-  let jsonStringify: (any) => string
+  // let jsonStringify: (any) => string
   let webAppManifestOutputPath: string
 
   return [
@@ -17,9 +19,10 @@ export function viteIconduitPlugin (options: ViteIconduitOptions): Plugin[] {
       name: 'vite-plugin-iconduit',
 
       async configResolved (config) {
+        assetsDir = config.build.assetsDir
         manifestPath = resolve(config.root, options.manifestPath)
-        jsonStringify = value => JSON.stringify(value, null, config.mode === 'production' ? 0 : 2)
-        webAppManifestOutputPath = join(config.build.assetsDir, 'app.webmanifest')
+        // jsonStringify = value => JSON.stringify(value, null, config.mode === 'production' ? 0 : 2)
+        webAppManifestOutputPath = join(assetsDir, 'app.webmanifest')
       },
 
       async buildStart () {
@@ -39,12 +42,22 @@ export function viteIconduitPlugin (options: ViteIconduitOptions): Plugin[] {
   async function processWebAppManifest (context, webAppManifestPath: string|null): void {
     if (typeof webAppManifestPath !== 'string') return
 
+    const webAppManifestDirPath = dirname(webAppManifestPath)
     const webAppManifest = JSON.parse(await readFile(webAppManifestPath))
+    const {icons = [], screenshots = []} = webAppManifest
+    const images = [...icons, ...screenshots]
 
-    context.emitFile({
-      type: 'asset',
-      fileName: webAppManifestOutputPath,
-      source: jsonStringify(webAppManifest),
-    })
+    return await Promise.all(images.map(async image => {
+      const srcUrl = urlParse(image.src)
+      const imagePath = resolve(webAppManifestDirPath, srcUrl.pathname)
+
+      const referenceId = context.emitFile({
+        type: 'asset',
+        name: basename(imagePath),
+        source: await readFile(imagePath),
+      })
+
+      return {referenceId, srcUrl}
+    }))
   }
 }
